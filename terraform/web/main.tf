b5965d9d907fde3cfbc12cc1dd8520e464f709bc
CalNHobsGotU <calnhobsgotu@CalNHobsGotUs-MacBook.local>
2022-10-10 10:29:12 -0400

    initial set-up, teraform dir with Jenkins and Webserver dir, ansible dir

---
+++ terraform/web/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "use-east-1"
+}
+
+variable "name" {
+    description = "name description on deploy"
+}
+
+resource "aws_instance" "FS_ec2_web" {
+    ami = "ami-08c40ec9ead489470"
+    instance_type = "t2.micro"
+    key_name = "devops-1"
+
+    tags = {
+        Name = "${var.name}"
+    }
+}
